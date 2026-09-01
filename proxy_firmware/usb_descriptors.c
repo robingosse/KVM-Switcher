7354a04170237f9bd1528da75bc43565540f083a
Robin Gosse <robingosse2@gmail.com>
2026-09-01 03:20:44 -0230

    Add USB HID keyboard descriptors

---
+++ proxy_firmware/usb_descriptors.c
@@ -0,0 +1,88 @@
+#include <string.h>
+#include "tusb.h"
+
+#define USB_VID 0xCAFE
+#define USB_PID 0x4010
+#define USB_BCD 0x0100
+
+static tusb_desc_device_t const desc_device = {
+    .bLength = sizeof(tusb_desc_device_t),
+    .bDescriptorType = TUSB_DESC_DEVICE,
+    .bcdUSB = 0x0200,
+    .bDeviceClass = 0x00,
+    .bDeviceSubClass = 0x00,
+    .bDeviceProtocol = 0x00,
+    .bMaxPacketSize0 = CFG_TUD_ENDPOINT0_SIZE,
+    .idVendor = USB_VID,
+    .idProduct = USB_PID,
+    .bcdDevice = USB_BCD,
+    .iManufacturer = 0x01,
+    .iProduct = 0x02,
+    .iSerialNumber = 0x03,
+    .bNumConfigurations = 0x01,
+};
+
+uint8_t const *tud_descriptor_device_cb(void) {
+    return (uint8_t const *)&desc_device;
+}
+
+uint8_t const desc_hid_report[] = {
+    TUD_HID_REPORT_DESC_KEYBOARD()
+};
+
+uint8_t const *tud_hid_descriptor_report_cb(uint8_t instance) {
+    (void)instance;
+    return desc_hid_report;
+}
+
+enum {
+    ITF_NUM_HID = 0,
+    ITF_NUM_TOTAL
+};
+
+#define CONFIG_TOTAL_LEN (TUD_CONFIG_DESC_LEN + TUD_HID_DESC_LEN)
+#define EPNUM_HID 0x81
+
+uint8_t const desc_configuration[] = {
+    TUD_CONFIG_DESCRIPTOR(1, ITF_NUM_TOTAL, 0, CONFIG_TOTAL_LEN, 0x00, 100),
+    TUD_HID_DESCRIPTOR(ITF_NUM_HID, 0, HID_ITF_PROTOCOL_KEYBOARD,
+                       sizeof(desc_hid_report), EPNUM_HID,
+                       CFG_TUD_HID_EP_BUFSIZE, 5),
+};
+
+uint8_t const *tud_descriptor_configuration_cb(uint8_t index) {
+    (void)index;
+    return desc_configuration;
+}
+
+static char const *string_desc_arr[] = {
+    (const char[]){0x09, 0x04},
+    "Gosse & Co.",
+    "KVM Keyboard Proxy",
+    "KVMRP2040",
+};
+
+static uint16_t desc_str[32];
+
+uint16_t const *tud_descriptor_string_cb(uint8_t index, uint16_t langid) {
+    (void)langid;
+    uint8_t count;
+
+    if (index == 0) {
+        memcpy(&desc_str[1], string_desc_arr[0], 2);
+        count = 1;
+    } else {
+        if (index >= (sizeof(string_desc_arr) / sizeof(string_desc_arr[0]))) {
+            return NULL;
+        }
+        char const *str = string_desc_arr[index];
+        count = (uint8_t)strlen(str);
+        if (count > 31) count = 31;
+        for (uint8_t i = 0; i < count; ++i) {
+            desc_str[1 + i] = (uint16_t)str[i];
+        }
+    }
+
+    desc_str[0] = (uint16_t)((TUSB_DESC_STRING << 8) | (2 * count + 2));
+    return desc_str;
+}
